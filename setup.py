4e40d17e85c16a637fbca806a58144c22af483e9
PowerBroker2 <gitstuff2@gmail.com>
2025-01-29 22:38:19 -0500

    Fix version num

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setup(
     name             = 'intermag',
     packages         = ['intermag'],
-    version          = '0.0.1',
+    version          = '0.0.2',
     description      = 'Python library to auto-download, manipulate, and process magnetic data from INTERMAGNET sites',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
     url              = 'https://github.com/PowerBroker2/intermag',
-    download_url     = 'https://github.com/PowerBroker2/intermag/archive/0.0.1.tar.gz',
+    download_url     = 'https://github.com/PowerBroker2/intermag/archive/0.0.2.tar.gz',
     keywords         = ['War Thunder'],
     classifiers      = [],
     install_requires = ['numpy', 'pandas', 'matplotlib', 'scipy', 'ppigrf']
